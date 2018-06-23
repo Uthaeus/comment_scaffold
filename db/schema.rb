633c3304096c8817a14c14cba2abadc33c2dbd30
Uthaeus <romanlavery@gmail.com>
2018-06-23 00:02:43 -0600

    Updated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623042859) do
+ActiveRecord::Schema.define(version: 20180623051224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,14 @@ ActiveRecord::Schema.define(version: 20180623042859) do
     t.string "title"
     t.string "subtitle"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "test_ads", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
